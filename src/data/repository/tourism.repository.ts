b4b71b38e948d0056831b7e669cc2bb577babab0
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2023-12-28 21:46:32 +0700

    fix category

--- src/data/repository/tourism.repository.ts
+++ src/data/repository/tourism.repository.ts
@@ -29,11 +29,11 @@ export default class TourismRepository {
             })
         })
     }
-    getByCategory = (id: number): Promise<Tourism> => {
+    getByCategory = (id: number): Promise<Tourism[]> => {
         return new Promise((resolve, reject) => {
             connection.query<Tourism[]>("SELECT * FROM tourism_with_id WHERE Id_Category = ?", [id], (err, res) => {
                 if (err) reject(err);
-                else resolve(res?.[0]);
+                else resolve(res);
             })
         })
     }

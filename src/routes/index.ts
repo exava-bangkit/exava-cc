439b9c1e8055c1bc5292bd2cd1a7f3a73c0ec129
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2023-12-28 16:46:08 +0700

    add profile endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import indexRoute from './endpoints'
-import {loginRoutes, registerRoutes} from "./endpoints/auth"
+import {loginRoutes, profileRoutes, registerRoutes} from "./endpoints/auth"
 import {tourismRoute, tourismByIdRoute, searchTourism, tourismRatingRoute} from './endpoints/tourism'
 
 export default [].concat(
     indexRoute,
     loginRoutes,
     registerRoutes,
+    profileRoutes,
     searchTourism,
     tourismRoute,
     tourismRatingRoute,
